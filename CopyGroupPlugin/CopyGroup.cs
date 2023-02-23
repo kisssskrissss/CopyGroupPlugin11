d5d2cdfa91f17ee999906453d2fb43cfe3435f6d
krist <krist@LAPTOP-HIQLHLL6>
2023-02-24 00:09:08 +0500

    Добавьте файлы проекта.

---
+++ CopyGroupPlugin/CopyGroup.cs
@@ -0,0 +1,88 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Architecture;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+
+namespace CopyGroupPlugin
+{
+    [TransactionAttribute(TransactionMode.Manual)]
+    public class CopyGroup : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            try
+            {
+                UIDocument uiDoc = commandData.Application.ActiveUIDocument;
+                Document doc = uiDoc.Document;
+
+                GroupPickFilter groupPickFilter = new GroupPickFilter();
+                Reference reference = uiDoc.Selection.PickObject(ObjectType.Element,groupPickFilter, "Выберите группу объектов");
+                Element element = doc.GetElement(reference);
+                Group group = element as Group;
+                XYZ groupCenter = GetElementCenter(group);
+                Room room = GetRoomByPoint(doc, groupCenter);
+                XYZ roomCenter = GetElementCenter(room);
+                XYZ offset = groupCenter - roomCenter;
+
+                XYZ point = uiDoc.Selection.PickPoint("Выберите точку");
+                Room pointRoom = GetRoomByPoint(doc,point);
+                XYZ newRoomCenter = GetElementCenter(pointRoom);
+
+                Transaction transaction = new Transaction(doc);
+                transaction.Start("Копирование группы объектов");
+                doc.Create.PlaceGroup(newRoomCenter+offset, group.GroupType);
+                transaction.Commit();
+            }
+            catch (Autodesk.Revit.Exceptions.OperationCanceledException)
+            {
+                return Result.Cancelled;
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message;
+                return Result.Failed;
+            }
+
+            return Result.Succeeded;
+        }
+
+        public Room GetRoomByPoint(Document doc, XYZ point)
+        {
+            FilteredElementCollector collector = new FilteredElementCollector(doc);
+            collector.OfCategory(BuiltInCategory.OST_Rooms);
+            foreach (Element e in collector)
+            {
+                Room room = e as Room;
+                if (room!=null)
+                {
+                    if (room.IsPointInRoom(point))
+                    {
+                        return room;
+                    }
+                }
+            }
+            return null;
+        }
+
+        public XYZ GetElementCenter (Element element)
+        {
+            BoundingBoxXYZ bounding =  element.get_BoundingBox(null);
+            return (bounding.Max + bounding.Min) / 2;
+        }
+
+        public class GroupPickFilter : ISelectionFilter
+        {
+            public bool AllowElement(Element elem)
+            {
+                return elem.Category.Id.IntegerValue == (int)BuiltInCategory.OST_IOSModelGroups;
+            }
+
+            public bool AllowReference(Reference reference, XYZ position)
+            {
+                return false;
+            }
+        }
+    }
+}
